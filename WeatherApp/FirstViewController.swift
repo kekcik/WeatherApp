a1815149b5cea6728cf12422ceb33ee527f7f7ee
Ivan Trofimov <richard1997dog@gmail.com>
2016-10-17 19:57:13 +0300

    Initial Commit

---
+++ WeatherApp/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  WeatherApp
+//
+//  Created by Иван Трофимов on 17.10.16.
+//  Copyright © 2016 Иван Трофимов. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
