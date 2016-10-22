5b59d1f47c4e3780f3bf99c66bd83b6aaed7d47c
Ivan Trofimov <richard1997dog@gmail.com>
2016-10-22 23:42:10 +0300

    Add Api Key

---
+++ WeatherApp/ApiKeyHelper.swift
@@ -0,0 +1,13 @@
+//
+//  ApiKeyHelper.swift
+//  WeatherApp
+//
+//  Created by Иван Трофимов on 22.10.16.
+//  Copyright © 2016 Иван Трофимов. All rights reserved.
+//
+
+import Foundation
+
+class keys {
+    static var openWeatherMapApiKye = "392200f354789fd0d775f1f6fa4247c9"
+}
